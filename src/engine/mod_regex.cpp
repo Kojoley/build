d08db254334c4c9acaa80c4ac74a0b19bef37996
Nikita Kniazev <kniazev.nikita@gmail.com>
2024-05-05 21:50:39 +0300

    Revert "Add missing header for tolower function."
    This reverts commit 80ba6ff692b653f3bebf5bb047973956881e79c4.

--- src/engine/mod_regex.cpp
+++ src/engine/mod_regex.cpp
@@ -18,7 +18,6 @@ Distributed under the Boost Software License, Version 1.0.
 #include <numeric>
 #include <string>
 #include <vector>
-#include <cctype>
 
 using namespace b2;
 using namespace b2::regex;
