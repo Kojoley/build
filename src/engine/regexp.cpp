3c24e8f88d04ef562d0649d503bab0ec2100e751
Nikita Kniazev <kniazev.nikita@gmail.com>
2024-05-05 21:46:24 +0300

    Revert "Fix out-of-bounds access for some regex matches."
    This reverts commit 14526797b4837f12e712fd557aec29b7cdf81047.

--- src/engine/regexp.cpp
+++ src/engine/regexp.cpp
@@ -1105,7 +1105,7 @@ struct executor
 				scan = scan.substr(count);
 				break;
 			case EXACTLY:
-				while (!scan.empty() && *opnd == scan[0])
+				while (*opnd == scan[0])
 				{
 					count++;
 					scan = scan.substr(1);
